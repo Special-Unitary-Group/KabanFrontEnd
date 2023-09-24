108c28c22b5d21b4a246864ccabfe7bc81a68523
David Lázaro Fernández <57787993+David-Lazaro-Fernandez@users.noreply.github.com>
2023-09-24 12:05:28 +0000

    Build Fixes

--- reader/next.config.js
+++ reader/next.config.js
@@ -1,25 +1,27 @@
-const { nextui } = require("@nextui-org/react");
 
 /** @type {import('next').NextConfig} */
+const { nextui } = require("@nextui-org/react");
 
+/** @type {import('tailwindcss').Config} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'media.licdn.com',
-          },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
       },
-    content: [
-        // ...
-        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     ],
-    theme: {
-        extend: {},
-    },
-    darkMode: "class",
-    plugins: [nextui()],
+  },
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
 }
 
 module.exports = nextConfig
+
